7eda307ac334911cdac3ce2366bbc2df5274e44b
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-25 17:17:18 -0500

    vacations controller only used actions

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -9,10 +9,10 @@ class VacationsController < ApplicationController
     render json: vacation.to_json, status: 200
   end
 
-  def new
-    new_employee: Employee.new
-    render json: new_employee.to_json, status: 200
-  end
+  # def new
+  #   new_employee: Employee.new
+  #   render json: new_employee.to_json, status: 200
+  # end
 
   # def create  ##not sure on this one
   #   if params[**employee/id/vacation.date_taken**].nil? || params[**employee/id/vacation.days_left**].empty?
@@ -27,31 +27,31 @@ class VacationsController < ApplicationController
   #   end
   # end
 
-  def update      #do we even need this because it wouldn't show on the page as a choice if it wasn't valid
-    if Vacation.exists?(params[:employee_id])
-      vacation                = Vacation.find(params[:employee_id])
-      vacation.date_taken     = params.fetch(:vacation).fetch(:date_taken)
-      vacation.days_left      = params.fetch(:vacation).fetch(:days_left)
-      vacation.save
-      render json: vacation.to_json, status: 200
-    else
-      render json: { error_msg: 'Employee vacation record not found.', id: params[:id] }.to_json, status: 404
-    end
+  # def update      #do we even need this because it wouldn't show on the page as a choice if it wasn't valid
+  #   if Vacation.exists?(params[:employee_id])
+  #     vacation                = Vacation.find(params[:employee_id])
+  #     vacation.date_taken     = params.fetch(:vacation).fetch(:date_taken)
+  #     vacation.days_left      = params.fetch(:vacation).fetch(:days_left)
+  #     vacation.save
+  #     render json: vacation.to_json, status: 200
+  #   else
+  #     render json: { error_msg: 'Employee vacation record not found.', id: params[:id] }.to_json, status: 404
+  #   end
 
-  def error
-    render json: { error: 'There has been an error' }, status: 500
-  end
+  # def error
+  #   render json: { error: 'There has been an error' }, status: 500
+  # end
 
 
 
-  def destroy
-    if Vacation.exists?(params[:employee_id])
-      vacation = Vacation.find(params[:employee_sid])
-      vacation.destroy
-      render json: { message: "Vacation record deleted successfully." }, status: 200
-    else
-      render json: { error_msg: 'No vacation information found', id: params[:id] }.to_json, status: 404
-    end
-  end
-  end
+  # def destroy
+  #   if Vacation.exists?(params[:employee_id])
+  #     vacation = Vacation.find(params[:employee_sid])
+  #     vacation.destroy
+  #     render json: { message: "Vacation record deleted successfully." }, status: 200
+  #   else
+  #     render json: { error_msg: 'No vacation information found', id: params[:id] }.to_json, status: 404
+  #   end
+  # end
+  # end
 end
