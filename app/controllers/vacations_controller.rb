887a33a9799109518f9cad3e25a1bc4904877c75
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-24 19:41:28 -0500

    added relations, created index and show for tables

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,8 +1,12 @@
 class VacationsController < ApplicationController
   def index
+    vacations = Vacation.new
+    render json: vacation, status 200
   end
 
   def show
+    vacation = vacation.find(params[:id])
+    render json: employee, status 200
   end
 
   def new
