0edcd694faa6e3cbce0987f84897bc31030304a1
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-28 08:32:34 -0500

    testing create vacation days controller

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -4,15 +4,15 @@ class VacationsController < ApplicationController
     render json: all_vacations_for_employee.to_json, status: 200
   end
 
-  # def create_date
-  #   if WHAT??
-  #     updated_vacation                = Vacation.where(employee_id: params[:employee_id])
-  #     updated_vacation.date_taken     = params[:date_taken]
-  #     updated_vacation.save
-  #     render json: updated_vacation.to_json, status: 200
-  #   else
-  #     render json: { error_msg: 'Employee is not found.', id: params[:id] }.to_json, status: 404
-  #   end
+  def create_date
+      updated_vacation                = Vacation.where(employee_id: params[:employee_id])
+       if updated_vacation            == nil
+        render json: { error_msg: 'Employee is not found.', id: params[:id] }.to_json, status: 404
+        updated_vacation.date_taken   = params[:date_taken]
+        updated_vacation.save
+      render json: updated_vacation.to_json, status: 200
+    else
+  end
 
 
 
